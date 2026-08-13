c7be4450af2cea7a2a749f4474d821d1bbb7305a
AMEH JOHN BRYMO <john@gmail.com>
2026-08-13 18:17:35 +0100

    Use production API URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,9 @@ import {
    Legend,
 } from "recharts";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000/api";
 
 const getToken = () => localStorage.getItem("token");
 
