a979b6506b7cda725290ed572ed3728a718717ab
AMEH JOHN BRYMO <john@gmail.com>
2026-08-19 15:47:17 +0100

    Fix server event handling

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
 
